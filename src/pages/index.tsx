7dd1d7c91d749a0839e8b8a94d4da45c93041164
mm0012 <kws060527@gmail.com>
2025-05-16 09:01:56 +0900

    애니메이션 딜레이 조정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -109,7 +109,7 @@ export default function Home() {
             key="team-block"
             initial={{ opacity: 0, y: 100 }}
             animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.5, delay: 0.6 }}
+            transition={{ duration: 0.6, delay: 0.8, ease: "easeInOut" }}
             className="mt-0 w-full"
           >
             {/* 이후 콘텐츠 */}
